0d835632ab0711786a9652c8c475b8410de960a2
Javi <sanroman.javier@gmail.com>
2017-01-19 18:19:29 +0100

    There can exist the same slug for different classifier types

--- app/models/wor/classifier.rb
+++ app/models/wor/classifier.rb
@@ -6,27 +6,26 @@ class Wor::Classifier < ActiveRecord::Base
   has_many :classifier_posts
   has_many :posts, through: :classifier_posts, order: 'publication_date desc'
 
-  scope :categories, conditions: {classifier_type: 'category'}
-  scope :tags, conditions: {classifier_type: 'tag'}
+  scope :categories, conditions: { classifier_type: :category }
+  scope :tags, conditions: { classifier_type: :tag }
 
   after_create :after_create
-
 
   def update_slug(_slug)
     slug_sanitize = Wor::Slugs.sanitize(_slug)
     _version = 0
-    while Wor::Classifier.where("id<>? and slug=?", id, slug_sanitize).first
+
+    while Wor::Classifier.where("id<>? and slug=? and classifier_type=?", id, slug_sanitize, classifier_type).first
       _version += 1
-      slug_sanitize    = "#{slug_sanitize}-#{_version}" 
+      slug_sanitize = "#{slug_sanitize}-#{_version}"
     end
 
-    self.update_attributes({slug: slug_sanitize})
+    update_attributes(slug: slug_sanitize)
   end
-
 
   private
 
   def after_create
-    self.update_slug(name)
+    update_slug(name)
   end
 end
