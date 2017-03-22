59add0e42e44bd678fcf54f5cc22155a882c4e55
Javi <sanroman.javier@gmail.com>
2017-03-22 13:49:09 +0100

    Added post_type field to create and update posts API

--- app/controllers/wor/api/v1/posts_controller.rb
+++ app/controllers/wor/api/v1/posts_controller.rb
@@ -1,61 +1,60 @@
 class Wor::Api::V1::PostsController < Wor::Api::V1::BaseController
   def index
     @posts = Wor::Post.where(true)
-    @posts = @posts.where("title like ?", "%#{params[:title]}%")  if !params[:title].blank?
-    @posts = @posts.where("user_id=?", params[:user_id])          if !params[:user_id].blank?
-    @posts = @posts.where("status=?",  params[:status])           if !params[:status].blank?
-    @posts = @posts.where("date >= ?", params[:date_begin]) if !params[:date_begin].blank?
-    @posts = @posts.where("date <= ?", params[:date_end])   if !params[:date_end].blank?
+    @posts = @posts.where('title like ?', "%#{params[:title]}%") unless params[:title].blank?
+    @posts = @posts.where('user_id=?', params[:user_id]) unless params[:user_id].blank?
+    @posts = @posts.where('status=?',  params[:status]) unless params[:status].blank?
+    @posts = @posts.where('date >= ?', params[:date_begin]) unless params[:date_begin].blank?
+    @posts = @posts.where('date <= ?', params[:date_end]) unless params[:date_end].blank?
 
     _classifier_ids = []
     _classifier_count = 0
 
-    if !params[:category_id].blank?
+    unless params[:category_id].blank?
       _classifier_count += 1
       _classifier_ids << params[:category_id]
     end
 
-    if !params[:tag_id].blank?
+    unless params[:tag_id].blank?
       _classifier_count += 1
       _classifier_ids << params[:tag_id]
     end
 
-    if !params[:category_slug].blank?
+    unless params[:category_slug].blank?
       _classifier_count += 1
       c = Wor::Classifier.categories.find_by_slug(params[:category_slug])
       _classifier_ids << c.id if !c.nil?
     end
 
-    if !params[:tag_slug].blank?
+    unless params[:tag_slug].blank?
       _classifier_count += 1
       c = Wor::Classifier.tags.find_by_slug(params[:tag_slug])
       _classifier_ids << c.id if !c.nil?
     end
 
     if _classifier_ids.count > 0
-      @posts = @posts
-        .joins(:classifier_posts)
-        .where("#{Wor::ClassifierPost.table_name}.classifier_id IN (?)", _classifier_ids)
-        .group("#{Wor::Post.table_name}.id")
-        .having("count(#{Wor::ClassifierPost.table_name}.id)=?", _classifier_count)
+      @posts = @posts.joins(:classifier_posts)
+                     .where("#{Wor::ClassifierPost.table_name}.classifier_id IN (?)", _classifier_ids)
+                     .group("#{Wor::Post.table_name}.id")
+                     .having("count(#{Wor::ClassifierPost.table_name}.id)=?", _classifier_count)
     end
 
     @posts = @posts.order("#{Wor::Post.table_name}.date desc, #{Wor::Post.table_name}.created_at desc")
 
-    @pagination = Wor::Pagination.new({current_page: params[:page]})
-    @posts      = @posts.paginate(page: @pagination.current_page, per_page: 20).all
-    @pagination.total_pages           = @posts.total_pages
-    @pagination.total_items           = @posts.total_entries
-    @pagination.total_current_items   = @posts.size
-    @pagination.per_page              = @posts.per_page
+    @pagination = Wor::Pagination.new(current_page: params[:page])
+    @posts = @posts.paginate(page: @pagination.current_page, per_page: 20).all
+    @pagination.total_pages = @posts.total_pages
+    @pagination.total_items = @posts.total_entries
+    @pagination.total_current_items = @posts.size
+    @pagination.per_page = @posts.per_page
 
-    render_message({view: :index})#({view: :create, messages: ["Se ha creado el lead correctamente"]})
+    render_message(view: :index)#({view: :create, messages: ["Se ha creado el lead correctamente"]})
   end
 
   def show
     @post = Wor::Post.find(params[:id])
 
-    render_message({view: :show})
+    render_message(view: :show)
   end
 
   def update
@@ -67,11 +66,12 @@ class Wor::Api::V1::PostsController < Wor::Api::V1::BaseController
                         seo_description: params[:seo_description],
                         user_id: params[:user_id],
                         publication_date: params[:publication_date],
-                        layout: params[:layout] }
+                        layout: params[:layout],
+                        post_type: params[:post_type] }
 
     if params[:status] == Wor::Post::PUBLISHED && !@post.published?
-      attrs_to_update[:disqus_identifier]  = "#{request.base_url}/#{wor_engine.posts_path}?id=#{@post.id}" if @post.disqus_identifier.nil?
-      attrs_to_update[:permalink]          = "#{request.base_url}/#{wor_engine.posts_path}?id=#{@post.id}" if @post.permalink.nil?
+      attrs_to_update[:disqus_identifier] = "#{request.base_url}/#{wor_engine.posts_path}?id=#{@post.id}" if @post.disqus_identifier.nil?
+      attrs_to_update[:permalink] = "#{request.base_url}/#{wor_engine.posts_path}?id=#{@post.id}" if @post.permalink.nil?
     end
 
     if params[:status] == Wor::Post::PUBLISHED || params[:status] == Wor::Post::DRAFT
@@ -87,7 +87,7 @@ class Wor::Api::V1::PostsController < Wor::Api::V1::BaseController
     Wor::ClassifierPost.where("post_id=?", @post.id).destroy_all
     Wor::ClassifierPost.create(post_id: @post.id, classifier_id: params[:category_id]) if params[:category_id]
 
-    if !params[:tags].nil?
+    unless params[:tags].nil?
       params[:tags].each do |tag_name|
         tag_slug = Wor::Slugs.sanitize(tag_name)
         tag = Wor::Classifier.tags.find_by_slug(tag_slug)
@@ -98,45 +98,47 @@ class Wor::Api::V1::PostsController < Wor::Api::V1::BaseController
     end
 
     if @post.valid?
-      render_message({view: :show})
+      render_message(view: :show)
     else
       render_model_error(@post)
     end
   end
 
   def create
-    @post = Wor::Post.create({title: params[:title],
-                              content: params[:content],
-                              seo_description: params[:seo_description],
-                              user_id: (wor_current_user.nil? ? nil : wor_current_user.id),
-                              date: params[:date] || Time.now,
-                              layout: Wor.post_layouts[0],
-                              status: params[:status],
-                              publication_date: params[:publication_date]
-                            })
+    @post = Wor::Post.create(
+      title: params[:title],
+      content: params[:content],
+      seo_description: params[:seo_description],
+      user_id: (wor_current_user.nil? ? nil : wor_current_user.id),
+      date: params[:date] || Time.now,
+      layout: Wor.post_layouts[0],
+      status: params[:status],
+      publication_date: params[:publication_date],
+      post_type: params[:post_type]
+    )
 
-    @post.add_classifier(params[:category_slug], :category) if !params[:category_slug].blank?
+    @post.add_classifier(params[:category_slug], :category) unless params[:category_slug].blank?
 
-    if !params[:tags].nil?
+    unless params[:tags].nil?
       params[:tags].each do |tag|
         @post.add_classifier(tag, :tag)
       end
     end
 
-    render_message({view: :show})
+    render_message(view: :show)
   end
 
   def upload_cover_image
     @post = Wor::Post.find(params[:post_id])
     @post.upload_cover_image(params[:file]) if params[:file]
 
-    render_message({view: :show})
+    render_message(view: :show)
   end
 
   def destroy
     post = Wor::Post.find(params[:id])
     post.destroy
 
-    render_message({messages: ["Post eliminado correctamente"]})
+    render_message(messages: ['Post eliminado correctamente'])
   end
 end
